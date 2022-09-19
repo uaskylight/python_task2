e989d8cdb0e0ba40f12594b615f40ffca696c9a5
AlexT <uaskylight@gmail.com>
2022-09-19 20:40:20 +0300

    Initial commit

---
+++ task2.py
@@ -0,0 +1,17 @@
+
+marks = float(input("Enter your marks:"))
+if marks >= 5:
+    print("Mark: A")
+elif marks >= 4:
+    print("Mark: B")
+elif marks >= 3:
+    print("Mark: C")
+elif marks >= 2:
+    print("Mark: D")
+elif marks >= 1:
+    print("Mark: F")
+else:
+    print("Zero")
+
+
+
